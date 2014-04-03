27c7c4c974f051e70f1ce0db66cae9afab3e1446
Olesia <olesia.hr@gmail.com>
2014-04-04 02:23:05 +0300

    Migration file

---
+++ lists/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# encoding: utf8
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('text', models.TextField()),
+                ('list', models.ForeignKey(to_field='id', to='lists.List')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
